e2932ad7f54d72dff317051fed3708a13593518f
NinzXtrmz <ninakhaled2002@gmail.com>
2023-08-06 09:50:55 +0300

    shape

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css', '../../../../bootstrap.min.css'],
+  styleUrls: ['../../../../bootstrap.min.css', './footer.component.css'],
 })
 export class FooterComponent {}
